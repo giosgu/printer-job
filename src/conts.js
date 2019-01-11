82a69baa21f6ddbb5c56bec9ccd4e6be143a2a5b
GIOSA GUIDO <MT30886835@osdebinario.grupoosde>
2019-01-11 20:19:13 -0300

    sa

--- src/conts.js
+++ src/conts.js
@@ -2,6 +2,7 @@ const FOO = 5;
 const MAIL_TO = 'guido.giosa@gmail.com';
 const MAIL_FROM = 'mantenimiento.impresora.giosgu@gmail.com';
 const MAIL_SEC = '4r0s3rpm1';
+//ayfty752egy49@hpeprint.com
 
 module.exports = {
     FOO: FOO,
