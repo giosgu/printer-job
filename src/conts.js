8487ffc7d3edd1321dac9e4b92483c5bc09515e0
GIOSA GUIDO <MT30886835@osdebinario.grupoosde>
2019-01-11 20:09:29 -0300

    Main printer job

---
+++ src/conts.js
@@ -0,0 +1,11 @@
+const FOO = 5;
+const MAIL_TO = 'guido.giosa@gmail.com';
+const MAIL_FROM = 'mantenimiento.impresora.giosgu@gmail.com';
+const MAIL_SEC = '4r0s3rpm1';
+
+module.exports = {
+    FOO: FOO,
+    MAIL_TO:MAIL_TO,
+    MAIL_FROM:MAIL_FROM,
+    MAIL_SEC:MAIL_SEC
+};
