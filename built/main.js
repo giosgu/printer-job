283ad6c563380d844f5118eca82860f74a733062
GIOSA GUIDO <MT30886835@osdebinario.grupoosde>
2019-01-14 14:05:09 -0300

    Convirtiendo proyecto a typescript

---
+++ built/main.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+var conts = require('./conts');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: conts.MAIL_FROM,
+        pass: conts.MAIL_SEC
+    }
+});
+var mailOptions = {
+    from: conts.MAIL_FROM,
+    to: conts.MAIL_TO,
+    subject: 'Impresión del mes',
+    text: 'That was easy!',
+    attachments: [{
+            filename: 'license.txt',
+            path: 'https://raw.github.com/nodemailer/nodemailer/master/LICENSE'
+        }]
+};
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log('Email sent: ' + info.response);
+    }
+});
